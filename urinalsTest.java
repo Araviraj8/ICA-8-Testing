9f21badf0d93a62be43c49e3b1c29ddc5cb74c23
Arnav Raviraj <arnavraviraj@gmail.com>
2022-10-27 22:15:10 -0700

    added the test case

---
+++ urinalsTest.java
@@ -0,0 +1,17 @@
+package practice;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class urinalsTest {
+	
+	urinalsFunctions uvar;
+
+	@Test
+	void testGetCount() {
+		System.out.println("====== Arnav Raviraj == TEST TWO EXECUTED =======");
+		assertTrue(uvar.goodString(null));
+	}
+
+}
